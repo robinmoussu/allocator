e3c4997eb4958e2762e15683fd8383dcbbca7694
robin moussu <robin.moussu+git@gmail.com>
2014-10-02 10:39:34 +0000

    Meilleur typage des zones mémoire

--- src/mem.c
+++ src/mem.c
@@ -7,25 +7,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdint.h>
 #include "mem.h"
 
-// Le pool de mémoire contenant toutes les données que l'on va allouer
-void *zone_memoire = 0;
-
 // Une zone mémoire (voir explications plus bas)
-// La taille minimale alouable est celle d'un pointeur.
+// La taille minimale alouable est celle d'un pointeur sur un autre bloc.
 union bloc {
-    union record *next_record; // Chaine de zone mémoire disponible
-    void         *data;        // Les données lorsque la zone est allouée
+    union bloc *next_record; // Chaine de zone mémoire disponible lorsque la
+                             // zone est non allouée
+    void       *data;        // Les données lorsque la zone est allouée
+};
+#define MIN_SIZE_ALLOC sizeof(union bloc)
+
+struct allocated_space {
+    uint8_t    memory_pool[ALLOC_MEM_SIZE*MIN_SIZE_ALLOC];    // zone mémoire
+                                // disponible pour l'utilisateur
+    union bloc free_space[BUDDY_MAX_INDEX];               // zone mémoire
+                                // contenant les données utilisées par
+                                // l'algorithme d'alocation
 };
 
-// Le tableau de BUDDY_MAX_INDEX elements pointant sur les zones mémoire libres
-// de taille MIN_SIZE_ALLOC * (2 puissance n) (avec n <= BUDDY_MAX_INDEX).
+// allocated_spac est le pool de mémoire contenant toutes les données que l'on
+// va allouer
+//
+// Le tableau allocated_space->memory_pool de BUDDY_MAX_INDEX elements pointe
+// sur les zones mémoire libres de taille MIN_SIZE_ALLOC * (2 puissance n)
+// (avec n <= BUDDY_MAX_INDEX).
 //
 // Les blocs allouables ont une taille T(n) = MIN_SIZE_ALLOC * (2 puissance n).
 //
 // Les blocs allouables de taille T(n) sont regroupés dans une liste dont le
-// premier élément est free_space[n].
+// premier élément est allocated_space->free_space[n].
 //
 // Si free_space[n] = 0, alors il n'y a pas de blocs de taille T(n) disponible.
 // Si free_space[n] = A (avec A une adresse différente de 0), alors le bloc
@@ -35,32 +47,26 @@ union bloc {
 // élément de la chaine (dont le champ next_record == 0).
 //
 // Si un bloc est alloué, alors il ne sera plus présent ni dans le tableau
-// free_space, ni dans aucune des sous-chaines. Dans ce cas, seul le champ data
-// du bloc devient utile.
-union record *free_space = 0;
-
-#define MIN_SIZE_ALLOC sizeof(record)
-
-/* ecrire votre code ici */
+// memory_space->free_space, ni dans aucune des sous-chaines. Dans ce cas, seul
+// le champ data du bloc devient utile.
+static struct allocated_space *memory_space = 0;
 
 int mem_init()
 {
-    if (!zone_memoire) {
-        zone_memoire = (void *) malloc(ALLOC_MEM_SIZE*MIN_SIZE_ALLOC +
-                sizeof(record)*BUDDY_MAX_INDEX);
-        free_space   = zone_memoire + ALLOC_MEM_SIZE;
+    if (!memory_space) {
+        memory_space = (void *) malloc( sizeof(struct allocated_space) );
     }
-    if (zone_memoire == 0) {
+    if (memory_space == 0) {
         perror("mem_init:");
         return -1;
     }
 
     // À l'initialisation, seul un bloc de de taille maximal (faisant
-    // MIN_SIZE_ALLOC*BUDDY_MAX_INDEX octets) est disponible.
-    for(i = 0; i < BUDDY_MAX_INDEX - 2; i++) {
-        free_space[i] = (union record *) 0;
+    // MIN_SIZE_ALLOC*(2 puissance BUDDY_MAX_INDEX) octets) est disponible.
+    for(int i = 0; i < BUDDY_MAX_INDEX - 2; i++) {
+        memory_space->free_space[i].next_record = 0;
     }
-    free_space[BUDDY_MAX_INDEX] = zone_memoire;
+    memory_space->free_space[BUDDY_MAX_INDEX] = *((union bloc *) &(memory_space->memory_pool));
     return 0;
 }
 
@@ -81,7 +87,7 @@ int mem_destroy()
 {
     /* ecrire votre code ici */
 
-    free(zone_memoire);
-    zone_memoire = 0;
+    free(memory_space);
+    memory_space = 0;
     return 0;
 }
